58276b4692583e522fa275f71c82b9b79220dd49
Sharkmare <34294231+Sharkmare@users.noreply.github.com>
2019-09-19 20:57:04 +0200

    Improving upon foundation, implementing messages

--- vorehandling.js
+++ vorehandling.js
@@ -1,36 +1,39 @@
-timeout= 1*1000
+timeout= 5*1000
 mode=0
 modes = [0,1,2,3] //0 = firstrun (devour), 1 = sloshing (progress), 2 = succumb (end),3= escape
 organs = 
 [
   {name:"maw",damage:1,trapto:"throat",escapeto:"freedom",
   messages:[
-    {key:"pre",message:"text 1"},
-    {key:"mid",message:"text 1"},
-    {key:"post",message:"text 1"},
-    {key:"pre",message:"text 2"},
-    {key:"mid",message:"text 2"},
-    {key:"post",message:"text 2"}
+    {key:0,message:"`\n *The bot leans down and swiftly chomps you up in its jaws.*"},
+    {key:1,message:"\n text 1"},
+    {key:2,message:"\n text 1"},
+    {key:0,message:"`\n *The bot swiftly snatches you up in its hand dangling you over its jaws before dropping you in.*"},
+    {key:1,message:"\n text 2"},
+    {key:2,message:"\n text 2"},
+    {key:3,message:"\n You manage to climb free of the bots maw!"}
     ],escapechance:1,trapchance:8
   },
   {name:"throat",damage:2,trapto:"stomach",escapeto:"maw",
   messages:[
-    {key:"pre",message:"text 1"},
-    {key:"mid",message:"text 1"},
-    {key:"post",message:"text 1"},
-    {key:"pre",message:"text 2"},
-    {key:"mid",message:"text 2"},
-    {key:"post",message:"text 2"}
+    {key:0,message:"\n error text"},
+    {key:1,message:"\n *the strong muscles contract and squeeze around you as a single finger squeezes down on the buldge you made in the bots throat.*"},
+    {key:2,message:"\n *you feel your bodies limbs give in from exhaustion as you slide down the bots tight throat, dropping into the stomach without any stamina left.*"},
+    {key:0,message:"\n error text"},
+    {key:3,message:"\n error text"},
+    {key:1,message:"\n *the throat ripples and pulses around you, squeezing your body as you travel through it.*"},
+    {key:2,message:"\n *you feel your bodies limbs give in from exhaustion as you slide down the bots tight throat, dropping into the stomach without any stamina left.*"}
     ],escapechance:2,trapchance:5
   },
   {name:"stomach",damage:10,trapto:"stomach",escapeto:"throat",
   messages:[
-    {key:"pre",message:"text 1"},
-    {key:"mid",message:"text 1"},
-    {key:"post",message:"text 1"},
-    {key:"pre",message:"text 2"},
-    {key:"mid",message:"text 2"},
-    {key:"post",message:"text 2"}
+    {key:0,message:"\n error text"},
+    {key:1,message:"`\n *the stomach sloshes you about noisily*"},
+    {key:2,message:"`\n *the bot lays down on its gut, tightly squeezing you within, pressing the last bits of stamina out of your body~*"},
+    {key:0,message:"\n error text"},
+    {key:3,message:"\n error text"},
+    {key:1,message:"`\n*the stomach compacts around you and squeezes you tightly.*"},
+    {key:2,message:"`\n *your body succumbs to the bots gut as it lets out a triumphant belch~*"}
     ],escapechance:1,trapchance:9
   }
 ]
@@ -43,36 +46,43 @@ selected = "maw"
 function slosh(hp,o,s)
 { 
   healthbar = hpbar(hp)
-  console.log(healthbar)
-  if (!mode){modehandling(0,hp);mode=1}
-  if (selected == "freedom") {return mstate = modehandling(3,hp)}
-  if(!HpIsZero(hp)) {mstate = modehandling(1,hp)}
-  if(HpIsZero(hp)) {return mstate = modehandling(2,hp)}
+  organ = o.filter(e=>e.name == s)
+  //console.log(organ)
+  
+  if (s == "freedom") {return mstate = modehandling(3,hp,s,o)}
+  if(!HpIsZero(hp) && mode) {mstate = modehandling(1,hp,s,o)}
+  if (!mode){modehandling(0,hp,s,o);mode=1}
+  if(HpIsZero(hp)) {return mstate = modehandling(2,hp,s,o)}
   
 
 handleslosh(hp,o,s,mode) //restarts slosh process
 }
 
-function modehandling(mode=0,hp)
-{
+function modehandling(mode=0,hp,s,o)
+{organ = o.filter(e=>e.name == s);message = organ[0].messages.filter(e=>e.key==mode);
+  
   switch (mode)
   {
     case 0: 
+    
+    
       console.log("Encounter starts.")
-      console.log(`Entered ${selected}`)
-      return "start"
+      console.log(`Entered ${s}`)
+      return message
     case 1: 
+
       //console.log(`sloshing in ${selected}`)
       //console.log(`${hp} hp left`)
-      return "progress"
+      return message
     case 2: 
+
       console.log("You lost.")
       console.log(`${hp} hp left`)
-      return "loss"
+      return message
     case 3:
       console.log("You won.")
       console.log(`${hp} hp left`)
-      return "win"
+      return message
     default: 
       console.log("error")
       return "error"
@@ -81,7 +91,7 @@ function modehandling(mode=0,hp)
 }
 
 
-function messagehandler()
+function messagehandler() //verifying if user sent a message, possibly by object comparision or timestamping
 {
 var mymessage="this";
 var lastmessage="that";
@@ -122,21 +132,21 @@ td = sanity[0]; th = sanity[1]; tm = sanity[2];ts = sanity[3]
 
 function handleslosh(hp,o,s,mode) //Handles actual damage being taken as well as escape/trap
 {
-hp=hp-o.filter(e=>e.name == selected)[0].damage
-organ = o.filter(e=>e.name == selected)
+hp=hp-o.filter(e=>e.name == s)[0].damage
+organ = o.filter(e=>e.name == s)
 
 doweescape=Math.trunc(Math.random()*10)
-if (doweescape == organ[0].escapechance ) 
+if (doweescape == organ[0].escapechance )
 {
 
-  console.log("Moved from " + selected + " to " + organ[0].escapeto )
-selected=organ[0].escapeto
+  console.log("Moved from " + s + " to " + organ[0].escapeto )
+s=organ[0].escapeto
 }
 else if ( doweescape == organ[0].trapchance )
 {
 
-  console.log("Moved from " + selected + " to " + organ[0].trapto )
-selected=organ[0].trapto
+  console.log("Moved from " + s + " to " + organ[0].trapto )
+s=organ[0].trapto
 }
 
 setTimeout(function(){
