45259831cebe9c8c551779b71ad52102fe51ea6f
Sharkmare <34294231+Sharkmare@users.noreply.github.com>
2019-09-19 15:02:49 +0200

    Documentation and Self preservation

--- vorehandling.js
+++ vorehandling.js
@@ -1,45 +1,83 @@
-organs = [{"name":"maw","damage":1,"trapto":"throat","escapeto":"freedom","messages":[{"key":"pre","message":"text 1"},{"key":"mid","message":"text 1"},{"key":"post","message":"text 1"},{"key":"pre","message":"text 2"},{"key":"mid","message":"text 2"},{"key":"post","message":"text 2"}],"escapechance":1,"trapchance":8},{"name":"throat","damage":2,"trapto":"stomach","escapeto":"maw","messages":[{"key":"pre","message":"text 1"},{"key":"mid","message":"text 1"},{"key":"post","message":"text 1"},{"key":"pre","message":"text 2"},{"key":"mid","message":"text 2"},{"key":"post","message":"text 2"}],"escapechance":2,"trapchance":5},{"name":"stomach","damage":4,"trapto":"stomach","escapeto":"throat","messages":[{"key":"pre","message":"text 1"},{"key":"mid","message":"text 1"},{"key":"post","message":"text 1"},{"key":"pre","message":"text 2"},{"key":"mid","message":"text 2"},{"key":"post","message":"text 2"}],"escapechance":1,"trapchance":9}]
+timeout= 1*1000
+organs = 
+[
+  {name:"maw",damage:1,trapto:"throat",escapeto:"freedom",
+  messages:[
+    {key:"pre",message:"text 1"},
+    {key:"mid",message:"text 1"},
+    {key:"post",message:"text 1"},
+    {key:"pre",message:"text 2"},
+    {key:"mid",message:"text 2"},
+    {key:"post",message:"text 2"}
+    ],escapechance:1,trapchance:8
+  },
+  {name:"throat",damage:2,trapto:"stomach",escapeto:"maw",
+  messages:[
+    {key:"pre",message:"text 1"},
+    {key:"mid",message:"text 1"},
+    {key:"post",message:"text 1"},
+    {key:"pre",message:"text 2"},
+    {key:"mid",message:"text 2"},
+    {key:"post",message:"text 2"}
+    ],escapechance:2,trapchance:5
+  },
+  {name:"stomach",damage:4,trapto:"stomach",escapeto:"throat",
+  messages:[
+    {key:"pre",message:"text 1"},
+    {key:"mid",message:"text 1"},
+    {key:"post",message:"text 1"},
+    {key:"pre",message:"text 2"},
+    {key:"mid",message:"text 2"},
+    {key:"post",message:"text 2"}
+    ],escapechance:1,trapchance:9
+  }
+]
 prey = {hp:100}
+
+var healthbar;
+
 console.log(organs)
 hp=prey.hp
 selected = "stomach"
-function slosh(hp,o,s,mode=0,messages=0)
+function slosh(hp,o,s)
+{ //console.clear()//this purges the console.
+  healthbar = hpbar(hp)
+  console.log(healthbar)
+
+
+if (HpIsZero(hp)) return succumb(o,s)
+
+handleslosh(hp,o,s) //restarts slosh process
+}
+
+function messagehandler()
+{}
+
+function handleslosh(hp,o,s) //Handles actual damage being taken as well as escape/trap
 {
 hp=hp-o.filter(e=>e.name == selected)[0].damage
-if (HpIsZero(hp)) return succumb(o,s)
-else 
-{
-messages=messagehandler(o,s,mode)
-console.log("HP: " + messages[0].message)
+
+setTimeout(function(){
+
+slosh(hp,o,s)
+},timeout)
 }
 
+function hpbar(hp,mhpo=50,heartsymbol="█") //Generates an hp bar based on math
+{   
+    hpo=Math.trunc(hp/2); hearts = []; hearts.push("Hp: `")
+    for (i=0;i< hpo ;i++) {hearts.push(heartsymbol)}
+    hpo=mhpo-hpo
+    for (i=0;i< hpo ;i++) {hearts.push(" ")}
+    hearts.push("`")
+    return hearts.join("")
 }
-function messagehandler(o,s,mode)
-{console.log(1)
-if(!mode){
-organ=o.filter(e=>e.name == s)
-messages = o[0].messages.filter(m=>m.key=="pre")
-return messages
 
-}
-
-}
-function handleslosh()
-{
-
-}
-function hpvalidate()
-{
-
-}
-function HpIsZero(hp)
+function HpIsZero(hp) //Checks hp being 0. returns true if 0
 {
   return (hp<!0)
 }
-function succumb(o,s)
-{
+function succumb(o,s) //Handles "player" losing.
+{}
 
-}
 slosh(prey.hp,organs,selected) //Process()
-
-console.log(HpIsZero(hp))
